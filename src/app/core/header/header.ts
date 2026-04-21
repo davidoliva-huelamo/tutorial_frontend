acd36450d12111e935a46ec3a00c0e35fb9fb6e1
davidoliva-huelamo <david.oliva-huelamo@capgemini.com>
2026-04-21 13:26:04 +0200

    Code: Listado Simple, Listado Paginado y Listado Filtrado de Frontend | Angular

---
+++ src/app/core/header/header.ts
@@ -0,0 +1,23 @@
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  templateUrl: './header.html',
+  styleUrls: ['./header.scss']
+})
+
+    export class Header {
+
+    }
